22b67814a60547cdbdd15069e690ab22afa788c0
KrKOo <krkoo.123@gmail.com>
2019-08-14 17:53:11 -0400

    test commit

--- client/src/App.js
+++ client/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>
-          Edit <code>src/App.js</code> andb save to reload.
+          Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
