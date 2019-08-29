251f222e1b4a9f215e3499876c7d1dac9ee46f7b
KrKOo <krkoo.123@gmail.com>
2019-08-29 05:01:14 -0400

    Test change 2

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
   }
 
   render() {
-    return <div>I'M READY TO USE TdHE BACK END APIS! :-)</div>;
+    return <div>I'M READY TO USE TdHE BACdK END APIS! :-)</div>;
   }
 }
 
